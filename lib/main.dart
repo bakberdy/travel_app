b7dd89eaef84891a911028d329597241f738aac9
Bakberdi <bakberdy.ye@gmail.com>
2026-01-18 21:48:08 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/onboarding_page.dart';
+import 'package:travel_app/theme.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      home: OnboardingPage(),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    colorScheme: ColorScheme(
+      brightness: .light,
+      primary: Color(0xff176EF1),
+      onPrimary: Colors.white,
+      surfaceContainer: Color(0xffF3F8FE),
+      onSurfaceVariant: Color(0xffB8B8B8),
+      primaryContainer: Color(0xffF3F8FE),
+      onPrimaryContainer: Color(0xff176EF1),
+      secondary: Colors.white,
+      onSecondary: Color(0xffB8B8B8),
+      error: Colors.red,
+      onError: Colors.white,
+      surface: Colors.white,
+      onSurface: Colors.black,
+    ),
+    textTheme: TextTheme(
+      
+    )
+  );
+}
