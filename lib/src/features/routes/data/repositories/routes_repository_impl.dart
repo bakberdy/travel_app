28931be34fea9b0bbabc3055193759a980d418d8
Bakberdi <bakberdy.ye@gmail.com>
2026-01-26 22:56:36 +0500

    refactor(routes): remove unused import and update getRouteTypes to return Right with route types

--- lib/src/features/routes/data/repositories/routes_repository_impl.dart
+++ lib/src/features/routes/data/repositories/routes_repository_impl.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
-import 'package:travel_app/src/core/error/failures.dart';
 import 'package:travel_app/src/core/utils/typedef.dart';
 import 'package:travel_app/src/features/routes/domain/entities/route_type_entity.dart';
 import 'package:travel_app/src/features/routes/domain/repositories/routes_repository.dart';
@@ -10,7 +9,7 @@ class RoutesRepositoryImpl implements RoutesRepository {
   @override
   FutureEither<Set<RouteTypeEntity>> getRouteTypes() async {
     await Future.delayed(Duration(seconds: 2));
-    return Left(CancelledFailure());
-    // return Right({RouteTypeEntity.lake, RouteTypeEntity.waterfall});
+    // return Left(CancelledFailure());
+    return Right({RouteTypeEntity.lake, RouteTypeEntity.waterfall});
   }
 }
